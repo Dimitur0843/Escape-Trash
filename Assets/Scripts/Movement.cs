488b897ba93551ed0dc97658236a04abc4af3d7d
Dimitur0843 <dimitur0843@gmail.com>
2017-02-05 16:46:40 +0200

    Base files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(Vector3.right * horizontal);
+        transform.Translate(Vector3.forward * vertical);
+    }
+}
